14dcff3854cdbf79758afa1dc16eb098f9c3286e
ianczm <ianczm@live.co.uk>
2021-12-13 11:57:21 +0800

    Add exampletest and maven manifest
    Finish setting up Maven and getting started with JUnit5

---
+++ src/test/java/com/hcyic1/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.hcyic1;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    @Test
+    void shouldShowSimpleAssertion() {
+        Assertions.assertEquals(1,1);
+    }
+}
