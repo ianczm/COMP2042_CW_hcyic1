774818ab907a45c23f1ff612d077ac7b830c5840
ianczm <ianczm@live.co.uk>
2021-12-14 21:59:58 +0800

    Revert "Update final JavaDocs"
    This reverts commit 76acfac429428815bd0360171cc86d5223d7a8c5.

--- src/main/java/com/hcyic1/brickdestroy/leaderboards/LeaderboardsView.java
+++ src/main/java/com/hcyic1/brickdestroy/leaderboards/LeaderboardsView.java
@@ -6,10 +6,6 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import java.util.ArrayList;
 
-/**
- * The object that displays the leaderboards as
- * a table GUI.
- */
 public class LeaderboardsView {
 
     private static final String WINDOW_TITLE = "Brick Destroy High Scores";
@@ -27,17 +23,12 @@ public class LeaderboardsView {
 
     Leaderboards leaderboards;
 
-    /**
-     * Constructor that shows a table based on the
-     * leaderboard object received.
-     * @param leaderboards leaderboards to be displayed.
-     */
     public LeaderboardsView(Leaderboards leaderboards) {
         this.leaderboards = leaderboards;
         initHighScoreView();
     }
 
-    private void initHighScoreView() {
+    public void initHighScoreView() {
 
         frame = new JFrame();
         frame.setTitle(WINDOW_TITLE);
@@ -50,7 +41,7 @@ public class LeaderboardsView {
         frame.add(scrollPane);
     }
 
-    private void updateHighScoreView() {
+    public void updateHighScoreView() {
         String[][] highScoreStrings = generateHighScoreStrings();
         DefaultTableModel tableData = new DefaultTableModel(highScoreStrings, HEADERS);
         table.setModel(tableData);
@@ -67,9 +58,6 @@ public class LeaderboardsView {
         return highScoreStrings.toArray(new String[0][]);
     }
 
-    /**
-     * Displays the leaderboards as a table.
-     */
     public void showScoreTable() {
         updateHighScoreView();
         frame.pack();
--- src/main/java/com/hcyic1/brickdestroy/leaderboards/UsernameInput.java
+++ src/main/java/com/hcyic1/brickdestroy/leaderboards/UsernameInput.java
@@ -3,10 +3,6 @@ package com.hcyic1.brickdestroy.leaderboards;
 import javax.swing.*;
 import java.awt.*;
 
-/**
- * The controller for username input that is tied directly
- * to one score/player object.
- */
 public class UsernameInput {
 
     private static final int WINDOW_WIDTH = 300;
@@ -16,10 +12,6 @@ public class UsernameInput {
 
     private final Score player;
 
-    /**
-     * Attaches this input controller to a score/player object.
-     * @param player the score object that requires username input.
-     */
     public UsernameInput(Score player) {
 
         this.player = player;
@@ -28,9 +20,6 @@ public class UsernameInput {
 
     }
 
-    /**
-     * Pops up a dialog for username entry.
-     */
     public void updateUsername() {
         player.setName(
             (String) JOptionPane.showInputDialog(
